e7f59700c7e49eac0b69baecfeb2fd270fc39fa3
Ravjot Brar <Ravjot.Brar@MacBookPro.lan>
2025-09-16 11:34:44 -0700

    Make selector inline

--- apps/frontend/src/state/valkey-features/info/infoSelectors.ts
+++ apps/frontend/src/state/valkey-features/info/infoSelectors.ts
@@ -1,9 +1,7 @@
 import type { RootState } from "@/store.ts";
 import { VALKEY } from "@common/src/constants.ts"
 
-export const selectData = (id: string) => (state: RootState) => {
-
-    return state[VALKEY.STATS.name]?.[id]?.data ?? null
-}
+export const selectData = (id: string) => (state: RootState) =>
+    state[VALKEY.STATS.name]?.[id]?.data ?? null
 
 
