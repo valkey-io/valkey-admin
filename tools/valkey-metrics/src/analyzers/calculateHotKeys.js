3c76868f31d46e12b55d232116566d50d570897a
Ravjot Brar <Ravjot.Brar@MacBookPro.lan>
2025-11-12 16:25:15 -0800

    Fix indentation
    Signed-off-by: Ravjot Brar <Ravjot.Brar@MacBookPro.lan>

--- tools/valkey-metrics/src/analyzers/calculateHotKeys.js
+++ tools/valkey-metrics/src/analyzers/calculateHotKeys.js
@@ -6,20 +6,19 @@ export const calculateHotKeys = () => {
     const ACCESS_COMMANDS = ["get", "set", "mget", "hget", "hgetall", "hmget", "json.get", "json.mget"]
     const CUT_OFF_FREQUENCY = 1
 
-    return R.pipe(
+  return R.pipe(
     R.reduce((acc, { ts, command }) => {
-        const [cmd, ...args] = command.split(" ").filter(Boolean)
-        
-        if (ACCESS_COMMANDS.includes(cmd.trim().toLowerCase())) { 
+      const [cmd, ...args] = command.split(' ').filter(Boolean)
+      if (ACCESS_COMMANDS.includes(cmd.trim().toLowerCase())) {
         args.forEach(key => {
-            acc[key] = acc[key] ? acc[key] + 1 : 1
+          acc[key] = acc[key] ? acc[key] + 1 : 1
         })
-        }
-        return acc
+      }
+      return acc
     }, {}),
     R.toPairs,
     R.sort(R.descend(R.last)),
-    R.reject(([key, count]) => count <= CUT_OFF_FREQUENCY),
-    )(rows)
+    R.reject(([key, count]) => count <= CUT_OFF_FREQUENCY)
+  )(rows)
 }
 
