5dbe6528d2f6c0aa1bfc63ab142ce60117d503a0
Ravjot Brar <Ravjot.Brar@VAN-FM2FQ6NHXT.local>
2025-12-08 14:51:16 -0800

    Add comment

--- apps/metrics/src/utils/constants.js
+++ apps/metrics/src/utils/constants.js
@@ -1,3 +1,5 @@
+// TODO: merge with common/src/constants.ts
+
 export const MONITOR = "monitor"
 export const COMMANDLOG_SLOW = "commandlog_slow"
 export const COMMANDLOG_LARGE_REPLY = "commandlog_large_reply"
