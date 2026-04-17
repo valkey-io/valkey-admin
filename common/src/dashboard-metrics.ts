3fdf8dc407ca988d0d8c1c295c1c969cfd17595c
Ravjot Brar <ravjot.brar@improving.com>
2026-04-17 13:05:31 -0700

    Add back total_system_memory descriptor
    Signed-off-by: Ravjot Brar <ravjot.brar@improving.com>

--- common/src/dashboard-metrics.ts
+++ common/src/dashboard-metrics.ts
@@ -40,8 +40,12 @@ export const singleMetricDescriptions = {
     description: "Number of bytes overhead by the EVAL scripts + Number of bytes overhead by Function scripts (part of used_memory).",
     unit: "Unit: Bytes",
   },
-  maxmemory: {
+  max_memory: {
     description: "Maximum amount of memory allocated for the Valkey instance.",
+    unit: "Unit: Bytes",
+  },
+  total_system_memory: {
+    description: "Total amount of memory that the Valkey instance has available.",
     unit: "Unit: Bytes",
   },
 
