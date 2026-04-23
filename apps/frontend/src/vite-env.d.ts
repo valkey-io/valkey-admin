33f6cd5aa883a0fcae63c05da8fa4ed7d9140514
Argus Li <contactme@chunkeili.com>
2026-04-23 10:11:18 -0700

    Set proper env access
    Signed-off-by: Argus Li <contactme@chunkeili.com>

--- apps/frontend/src/vite-env.d.ts
+++ apps/frontend/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_VALKEY_ADMIN_WS_URL?: string
   readonly VITE_LOCAL_VALKEY_HOST?: string
   readonly VITE_LOCAL_VALKEY_PORT?: string
   readonly VITE_LOCAL_VALKEY_NAME?: string
