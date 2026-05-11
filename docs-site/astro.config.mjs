81f307d7697cab681e6007a8e597567052d689be
Argus Li <contactme@chunkeili.com>
2026-05-11 14:12:52 -0700

    Add site and base values
    Signed-off-by: Argus Li <contactme@chunkeili.com>

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://valkey-io.github.io',
+	base: '/valkey-admin',
 	integrations: [
 		starlight({
 			title: 'Valkey Admin',
