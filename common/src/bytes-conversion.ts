acd05ba76295a7984a7b761bc90d3dafcd4efc54
ravjotb <ravjot.brar@improving.com>
2026-06-19 16:24:42 -0700

    Change to em dash
    Signed-off-by: ravjotb <ravjot.brar@improving.com>

--- common/src/bytes-conversion.ts
+++ common/src/bytes-conversion.ts
@@ -1,5 +1,5 @@
 export function formatBytes(bytes: number) {
-  if (bytes < 0) return "-"
+  if (bytes < 0) return "—"
   if (bytes === 0) return "0 B"
   const k = 1024
   const sizes = ["B", "KB", "MB", "GB"]
